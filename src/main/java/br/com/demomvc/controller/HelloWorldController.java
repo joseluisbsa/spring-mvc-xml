56024a33ae1ef472072b70d4b5dc2221d2e4dd34
Joseph <jose.sa@metasix.com.br>
2019-08-26 20:41:00 -0300

    spring mvc 4.3.25 config em xml

---
+++ src/main/java/br/com/demomvc/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package br.com.demomvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+
+}
