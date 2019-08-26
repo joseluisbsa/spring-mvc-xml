fb991be8d7439555a5b43a14b83eccc6d8050d87
Joseph <jose.sa@metasix.com.br>
2019-08-26 20:56:00 -0300

    alterado do projeto para springmvcxml

--- src/main/java/br/com/demomvcxml/controller/HelloWorldController.java
+++ src/main/java/br/com/demomvcxml/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package br.com.demomvc.controller;
+package br.com.demomvcxml.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
